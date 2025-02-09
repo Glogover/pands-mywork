01c36743ce3358864e5303b652122a9c03d7b41e
Marcin Kaminski <marcin442228@gmail.com>
2025-02-09 18:53:51 +0000

    week 02

---
+++ week02/nameAndAge.py
@@ -0,0 +1,12 @@
+# nameAndAge.py
+# Program that reads in a name and age and outputs "Hello {name}, your age is {age}"
+# Author: Marcin Kaminski
+
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+print(f"Hello {name}, \nyour age is {age}.")
+
+print(21-4) # Outputs 21 -4
+
+print(2 == 3) # Outputs if 2 = 3
