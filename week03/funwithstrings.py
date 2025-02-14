ecb4eecd815de3e1994ed9d8552807fc19585d9e
Marcin Kaminski <marcin442228@gmail.com>
2025-02-14 00:53:10 +0000

    week 03

---
+++ week03/funwithstrings.py
@@ -0,0 +1,22 @@
+# funwithstrings.py
+# Author: Marcin Kaminski
+# This is just me messing with strings
+# showing:
+#  Escape characters
+#  Adding numbers to strings
+#  splicing
+
+astring = "He said \"hello\""
+print(astring)
+
+mark = 33
+string2 = "Your mark is: " + str(mark)
+print(string2)
+
+accountno = "123456789"
+print(accountno[0:4])
+print(accountno[1:])
+
+
+
+
---
+++ week03/messingwithnumbers.py
@@ -0,0 +1,8 @@
+# messing with numbers
+# Author: Marcin Kaminski
+
+afloat = .29
+ascent = afloat * 100
+
+print(f"{afloat} as cent is {ascent}")
+
