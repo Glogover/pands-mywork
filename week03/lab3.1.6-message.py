6f5030e3aabdaf67b70e3f3025db2f369aa7f04a
Marcin Kaminski <marcin442228@gmail.com>
2025-02-14 00:06:44 +0000

    week 03

---
+++ week03/lab3.1.6-message.py
@@ -0,0 +1,22 @@
+# message.py
+# Author: Marcin Kaminski
+
+message = 'I have eaten ' + str(99) + ' burritos' # We can only concatenate string to string
+print(message)
+
+""" 
+
+Question 7: 
+Why is eggs a valid variable name while 100 is invalid?  
+
+Answer: 
+100 is an invalid variable name as we cannot start a variable name with a number.
+
+Question 8: 
+What three functions can be used to get the integer, floating-point number, or
+string version of a value?
+
+Answer: 
+int(), float(), str()
+
+"""
