85780dc9ebf42528db8d38505577ef99c2089c18
Marcin Kaminski <marcin442228@gmail.com>
2025-02-25 22:38:17 +0000

    Lab 3.3 Strings

---
+++ week03/3.3.1-length.py
@@ -0,0 +1,8 @@
+# length.py
+# This program reads in a string and outputs its length.
+# Author: Marcin Kaminski
+
+inputString = input("enter a string: ")
+lengthOfString = len(inputString)
+print(f"The length of {inputString} is {lengthOfString} characters")
+
---
+++ week03/3.3.2-string.py
@@ -0,0 +1,7 @@
+# string.py
+# Simple program creating and outputting a string
+# Author: Marcin Kaminski
+
+message = 'John said \t"hi"\nI said\t\t"bye"'
+print(message)
+
