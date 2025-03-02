9184f9e5175d0076997f46fae919089178a1ee7d
Marcin Kaminski <marcin442228@gmail.com>
2025-03-02 17:46:16 +0000

    Week05

---
+++ week05/messingWithDict2.py
@@ -0,0 +1,18 @@
+# Messing with dictionaries 2
+# Author: Marcin Kaminski
+
+car = {
+   "make" : "Fiat",
+   "model" : "Punto",
+   "price" : 10000,
+   "tags" : ["pre-owned", "Best Buy", "Dealer"]
+ }
+
+#print(car)
+
+#for key in car:
+#   print(key, 'has value ', car[key])
+
+for key, value in car.items():
+    print(key, 'has a value', value)
+
