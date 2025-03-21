a13f9568dbe0589205d8c0f51055f94409669ac5
Marcin Kaminski <marcin442228@gmail.com>
2025-03-21 19:16:02 +0000

    Week06

--- week06/flexibleArguments.py
+++ week06/flexibleArguments.py
@@ -5,8 +5,29 @@
 print(1,2,3, sep="", end="\t")
 print("hi")
 
-def fun1(*args):
+# unnamed arguments (args)
+def fun1(*args): 
     print(type(args))
-    print(args)
+    #print(args)
+    for val in args:
+        print(val)
 
-fun1("a", "b", "c")
+#fun1("a", "b", "c")
+
+# keyword arguments (kwargs)
+def fun2(**kwargs): 
+    print(type(kwargs))
+    print(kwargs)
+    #for val in kwargs:
+    #   print(val)
+
+fun2(name="joe", age=21, gender="M") # dictionary
+
+# sample code
+
+
+
+
+    
+    
+
