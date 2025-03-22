a78460ff593f792e5466f698e5eb2421a413622c
Marcin Kaminski <marcin442228@gmail.com>
2025-03-22 20:37:41 +0000

    Week06

---
+++ week06/messingWithModules.py
@@ -0,0 +1,18 @@
+# Messing with modules
+# Author: Marcin Kaminski
+
+#from math import cos # Importing cosine function
+
+#import math as m # Importing the math module as m
+
+#var = m.cos(3.14) # Get cosine out of the math module
+
+#var = cos(3.14)
+
+#print(var)
+
+from constraint import *
+from github import Github
+import nltk
+
+
