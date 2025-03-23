b76706d02c3d9b483a9f85b3c8c1e630950a4d32
Marcin Kaminski <marcin442228@gmail.com>
2025-03-23 16:05:21 +0000

    Week06

---
+++ week06/6.2-student.py
@@ -0,0 +1,23 @@
+# student.py
+# Author: Marcin Kaminski
+
+def displayMenu():
+    print("What would you like to do?")
+    print("\t(a) Add new student")
+    print("\t(v) View students")
+    print("\t(q) Quit")
+    choice = input("Type one letter (a/v/q): ").strip()
+
+    return choice
+
+#test the function
+choice = displayMenu()
+print(f"you chose {choice}")
+
+
+
+
+
+
+
+    
