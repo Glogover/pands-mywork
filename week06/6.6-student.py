5f3a644d1a295043ddf7ae39317f6a72f7891b73
Marcin Kaminski <marcin442228@gmail.com>
2025-03-23 17:35:55 +0000

    Week06

---
+++ week06/6.6-student.py
@@ -0,0 +1,50 @@
+# 6.6-student.py
+# Author: Marcin Kaminski
+# the array we store everything in
+
+def displayMenu():
+    print("What would you like to do?")
+    print("\t(a) Add new student")
+    print("\t(v) View students")
+    print("\t(q) Quit")
+    choice = input("Type one letter (a/v/q): ").strip()
+    return choice
+
+def doAdd(students):
+    currentStudent = {}
+    currentStudent["name"]=input("enter name: ")
+    currentStudent["modules"]=readModules()
+    students.append(currentStudent)
+
+def readModules():
+    modules = []
+    moduleName = input("\tEnter the first Module name (blank to quit): ").strip()
+
+    while moduleName !="":
+        module = {}
+        module["name"]= moduleName
+        # I am not doing error handling
+        module["grade"]=int(input("\t\tEnter grade: "))
+        modules.append(module)
+        # now read the next module name
+        moduleName = input("\tEnter next module name (blank to quit): ").strip()
+
+    return modules
+
+def doView(students):
+    # we fill this in later
+    print(students)
+
+
+# main program
+
+students = []   
+choice = displayMenu()
+while (choice != "q"):
+    if choice == "a":
+        doAdd(students)
+    elif choice == "v":
+        doView(students)
+    elif choice != "q":
+        print("\n\nplease select either a, v or q")
+    choice = displayMenu()
