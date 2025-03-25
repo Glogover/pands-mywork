2594b49548b2ed62f30ddd4a654696cd55312c9d
Marcin Kaminski <marcin442228@gmail.com>
2025-03-25 00:12:50 +0000

    Week07

---
+++ week07/messingwithfiles.py
@@ -0,0 +1,28 @@
+# messing with files
+# Author: Marcin Kaminski
+
+FILENAME= "data.txt"
+
+#with open(FILENAME, "rt") as f:
+    #data = f.read()
+    #print(data)
+
+with open(FILENAME, "rt") as f:
+    for  data in f: # reading one line at a time
+          #print(data) 
+          #print(data, end="")
+          #print(data.strip() ) 
+          #print(data[:-1])
+
+with open("data2.txt", "wt") as f:
+       f.write("how now")
+       f.write("brown cow")
+
+print(done)
+
+
+
+
+
+
+
