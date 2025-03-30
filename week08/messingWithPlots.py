f8ad4b70b6b88047f27e2aad99730c34f129b408
Marcin Kaminski <marcin442228@gmail.com>
2025-03-30 21:58:01 +0100

    Week08

---
+++ week08/messingWithPlots.py
@@ -0,0 +1,12 @@
+# messing with matplotlib
+# Author: Marcin Kaminski
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+xpoints = np.array(range(1,101)) # points from 1 to 100
+ypoints = xpoints * xpoints # x squared
+
+plt.plot(xpoints, ypoints)
+plt.show
+
