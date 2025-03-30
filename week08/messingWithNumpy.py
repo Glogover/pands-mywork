615226bc58c89159838adc051fb2e5f3f86f35bd
Marcin Kaminski <marcin442228@gmail.com>
2025-03-30 17:38:41 +0100

    Week08

---
+++ week08/messingWithNumpy.py
@@ -0,0 +1,20 @@
+# Messing with Numpy
+# Author: Marcin Kaminski
+
+import numpy as np
+
+list_of_numbers = [1,2,3, "asdf"]
+print(list_of_numbers)
+
+numbers = np.array([1,2,3,4]) # yu can only store one variable type inside numpy
+print(numbers)  # when printing no commas between numbers
+
+
+
+
+
+
+
+
+
+
