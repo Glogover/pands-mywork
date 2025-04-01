6d36247973aeb1cec807a2e5e755466c259ad1da
Marcin Kaminski <marcin442228@gmail.com>
2025-04-01 22:37:08 +0100

    Week08

--- week08/messingWithPlots.py
+++ week08/messingWithPlots.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 xpoints = np.array(range(1,101)) # points from 1 to 100
 ypoints = xpoints * xpoints # x squared
 
-plt.plot(xpoints, ypoints)
-plt.show()
+plt.plot(xpoints, ypoints, label="xsquared")
+plt.legend() 
+#plt.show() # Showing the figure
+plt.savefig('lecture1.png') # Saving the figure
 
 
