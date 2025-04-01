39006315bb52fddc9db02fee6c5a20cb9ed8331e
Marcin Kaminski <marcin442228@gmail.com>
2025-04-01 23:27:26 +0100

    Week08

--- week08/messingWithPlots.py
+++ week08/messingWithPlots.py
@@ -8,8 +8,13 @@ xpoints = np.array(range(1,101)) # points from 1 to 100
 ypoints = xpoints * xpoints # x squared
 
 plt.plot(xpoints, ypoints, label="xsquared")
-plt.legend() 
+plt.plot(xpoints, xpoints, label="straight", color="blue") # Plotting a straight blue line
+plt.legend()
+
+randompoints = np.random.randint(1, 1000, 100) # 100 random points ranging from 1 to 1000
+plt.scatter(xpoints, randompoints, label="random")
+
 #plt.show() # Showing the figure
-plt.savefig('lecture1.png') # Saving the figure
+plt.savefig('lecture3.png') # Saving the figure
 
 
