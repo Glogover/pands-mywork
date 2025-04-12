f68fab0c2fe5871564e873728753ffe7b2dc7429
Marcin Kaminski <marcin442228@gmail.com>
2025-04-12 15:48:56 +0100

    Week09

--- week09/myFunctions.py
+++ week09/myFunctions.py
@@ -8,6 +8,8 @@ def withdraw(amount):
     global balance
     if amount < 0:
         raise ValueError("amount should always be greater than 0")
+    if amount > balance:
+        raise ValueError("not enough funds")
 
     balance = balance - amount
     return balance
@@ -15,4 +17,19 @@ def withdraw(amount):
 if __name__ == "__main__":
     # this code will only run if the script is run directly
     # it will not run if the script is imported as a module
-    print("dont want this")
+    assert withdraw(20) == 80, "incorrect calculation"
+    try:
+        withdraw(-1)
+        assert False, "should have thrown a value error"
+    except ValueError as ve:
+        pass
+
+    try:
+        withdraw(110)
+        assert False, "can't wihdraw more than is in balance"
+    except ValueError as ve:
+        pass
+
+    print ("all good")
+
+
