2221ef9176474ed2a322a5a30e8a8c0e5eae8a23
Marcin Kaminski <marcin442228@gmail.com>
2025-04-12 15:19:52 +0100

    Week09

--- week09/myFunctions.py
+++ week09/myFunctions.py
@@ -11,3 +11,8 @@ def withdraw(amount):
 
     balance = balance - amount
     return balance
+
+if __name__ == "__main__":
+    # this code will only run if the script is run directly
+    # it will not run if the script is imported as a module
+    print("dont want this")
--- week09/testMyFunctions.py
+++ week09/testMyFunctions.py
@@ -3,6 +3,8 @@
 
 import myFunctions as mf
 
-print mf.withdraw(20)
+#print(mf.withdraw(-1))
+print(mf.withdraw(30))
+
 print("all good")
 
