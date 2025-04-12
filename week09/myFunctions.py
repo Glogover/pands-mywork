878aa36ad009c7ec05b2fa439f80d81052fe0f52
Marcin Kaminski <marcin442228@gmail.com>
2025-04-12 18:31:34 +0100

    Week09

--- week09/myFunctions.py
+++ week09/myFunctions.py
@@ -1,17 +1,26 @@
 # module of useful functions
 # Author: Marcin Kaminski
 
+import logging
+logging.basicConfig(filename="./bank.log",
+                    level=logging.DEBUG,
+                    format="%(asctime)s-%(levelname)s-%(message)s- %(lineno)d")
 
 balance = 100 # 100 cents = 1 euro
 
 def withdraw(amount):
     global balance
     if amount < 0:
+        logging.critical(f"the amount ({amount}) should never be less than 0")
+
         raise ValueError("amount should always be greater than 0")
     if amount > balance:
-        raise ValueError("not enough funds")
+        logging.warning(f"trying to withdraw more ({amount}) than is in account ({balance})")
 
+        raise ValueError("not enough funds")
     balance = balance - amount
+    logging.info(f"we have just withdrawn {amount}, new balance is {balance}")
+
     return balance
 
 if __name__ == "__main__":
