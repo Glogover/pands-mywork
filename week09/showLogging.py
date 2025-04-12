f6acd758d208f207a9a3b1bf98ed9ff3ddf13e8a
Marcin Kaminski <marcin442228@gmail.com>
2025-04-12 17:21:52 +0100

    Week09

---
+++ week09/showLogging.py
@@ -0,0 +1,8 @@
+# Demonstrate logging
+# Attributes you can put in the basicConfig:
+# level
+# filename
+# filemode
+# format
+#      %(name)s - %(levelname)s - %(message)s - %(asctime)s - %(lineno)d
+
